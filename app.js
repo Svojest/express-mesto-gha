4b313d452dc9020226dc421f64920cb9ae2c54a8
Svojest <svojest@yandex.ru>
2022-10-14 00:27:09 +0400

    13 практическая, 1 интерация (5 исправление)

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { ERROR_CODE, MESSAGE_ERROR } = require('./constans/errors');
+const { ERROR_CODE, ERROR_MESSAGE } = require('./constans/errors');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
@@ -21,7 +21,7 @@ app.use('/', require('./routes/cards'));
 app.use('/', require('./routes/users'));
 
 app.use((req, res) => {
-  res.status(ERROR_CODE.notFound).send({ message: MESSAGE_ERROR.notFound });
+  res.status(ERROR_CODE.notFound).send({ message: ERROR_MESSAGE.notFound });
 });
 
 app.listen(PORT, () => {
