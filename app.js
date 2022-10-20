3bbd7bf8c3d26ca080cc8986db8f5a606d2b135c
Svojest <svojest@yandex.ru>
2022-10-20 23:44:44 +0400

    14 практическая, 1 интерация (7 исправление)

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.post(
   '/signin',
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().required(),
+      email: Joi.string().email().required(),
       password: Joi.string().required(),
     }),
   }),
